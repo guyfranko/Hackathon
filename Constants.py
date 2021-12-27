dec108ce3ce1c5ab89f8c4fd8e66c9481036f310
guyfranko <guyfrankovits@gmail.com>
2021-12-27 18:06:29 +0200

    Initial commit

---
+++ Constants.py
@@ -0,0 +1,22 @@
+class Colors:
+    TITLE = '\x1b[41m'
+    Player_1 = '\x1b[1;31;40m'
+    Player_2 = '\x1b[1;34;40m'
+    NUM_CHARS = '\x1b[1;30;43m'
+    END_COLOR = '\x1b[0m'
+
+GROUP_NAME_1 = "Group 1"
+GROUP_NAME_2 = "Group 2"
+
+SERVER_PORT = 2110
+
+SECONDS_WAITING_FOR_CLIENTS = 10
+
+BROADCAST_PORT = 13117
+BROADCAST_IP = "172.1.255.255"
+
+MESSAGE_SIZE = 1024
+
+UNICODE = 'utf-8'
+
+TEAM_NAME = "Wet Assh Protocol"
