c27091c62e222a10effc6b07eb95369cf84b0bc4
Guy frankovits <83398876+guyfranko@users.noreply.github.com>
2021-12-30 00:15:45 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,20 +1,20 @@
 import socket
-import time
-import struct
 import multiprocessing
+import time
 import getch
+import struct
 
 
 class Client:
-    def __init__(self, TEST):
-        self.teamName = "DG-hac"
+    def _init_(self, TESTING):
         self.socketClientUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.socketClientUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        if TEST:
+        if TESTING:
             self.socketClientUDP.bind((BRODCAST_IP_TESTING, BROADCAST_PORT))
         else:
             self.socketClientUDP.bind((BRODCAST_IP_DEV, BROADCAST_PORT))
-        print("Client started, listening for offer requests...")
+        self.teamName = "DG-hac"
+        print("Client started listening for offer requests")
         self.socketClientTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.WaitForGame()
 
@@ -24,18 +24,17 @@ class Client:
             try:
                 data, addr = self.socketClientUDP.recvfrom(8)
                 data = struct.unpack('IbH', data)
-                serverPort = data[2]
                 if data[0] != 0xabcddcba:
                     continue
-                print(f"Received offer from {format(addr[0])}, attempting to connect...")
-                self.ConnectGame(addr[0], int(serverPort))
+                print(f"Received offer from {format(addr[0])}, attempting to connect")
+                self.ConnectGame(addr[0], int(data[2]))
             except:
                 pass
 
-    def ConnectGame(self, addr, gamePort):
+    def ConnectGame(self, addr, port):
         try:
             self.socketClientTCP.settimeout(10)
-            self.socketClientTCP.connect((addr, gamePort))
+            self.socketClientTCP.connect((addr, port))
             self.socketClientTCP.sendall((self.teamName + '\n').encode())
             data = None
             try:
@@ -43,22 +42,22 @@ class Client:
             except:
                 pass
             if data is None:
-                print('No welcome message received.')
-                raise Exception('unconnected to Server .')
+                print('No welcome message received')
+                raise Exception('unable to connect Server')
             else:
                 print(data.decode())
             self.RunGame()
-            print('Server disconnected, listening for offer requests...')
+            print('Server disconnected, listening for offer requests')
         except:
             pass
         self.socketClientTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def RunGame(self):
-        treads = multiprocessing.Process(target=self.getResult).decode()
-        treads.start()
-        treads.join(10)
-        if treads.is_alive():
-            treads.terminate()
+        playerAnswer = multiprocessing.Process(target=self.getResult)
+        playerAnswer.start()
+        playerAnswer.join(10)
+        if playerAnswer.is_alive():
+            playerAnswer.terminate()
         self.socketClientTCP.settimeout(2.5)
         msg = None
         try:
@@ -71,11 +70,11 @@ class Client:
             print(msg)
 
     def getResult(self):
-        stop_time = time.time() + 10
-        while time.time() < stop_time:
+        startTime = time.time()
+        while time.time() < startTime + 10:
             try:
-                char = getch.getch()
-                self.socketClientTCP.sendall(char.encode())
+                ans = getch.getch()
+                self.socketClientTCP.sendall(ans.encode())
             except:
                 pass
 
